345e7956cf336abd91212f7b718dd66f97f4ed45
Simon <simonwilli7@web.de>
2025-06-11 12:35:51 +0200

    Pagination vorbereitet

--- index.php
+++ index.php
@@ -3,6 +3,13 @@
 require_once __DIR__ . '/inc/db-connect.php';
 require_once __DIR__ . '/inc/functions.php';
 
+$stmtCount = $pdo->prepare('SELECT COUNT(*) AS `count` FROM `entries`');
+$stmtCount->execute();
+$countTotal = $stmtCount->fetch(PDO::FETCH_ASSOC)['count'];
+var_dump($countTotal);
+die();
+
+
 $stmt = $pdo->prepare('SELECT * FROM `entries`');
 $stmt->execute();
 $entries = $stmt->fetchAll(PDO::FETCH_ASSOC);
