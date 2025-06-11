7e2775e0cd448430450e8ad199391091155a121b
Simon <simonwilli7@web.de>
2025-06-11 12:49:25 +0200

    pagination 2

--- views/index.view.php
+++ views/index.view.php
@@ -1,25 +1,23 @@
 <!DOCTYPE html>
 <html lang="de">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    
     <link rel="stylesheet" type="text/css" href="./styles/lib/montserrat/webfonts/Montserrat.css" />
     <link rel="stylesheet" type="text/css" href="./styles/main.css" />
     <title>Gästebuch</title>
 </head>
-
 <body>
     <div class="container">
         <h1 class="guestbook-heading">Gästebuch</h1>
         <form method="POST" action="submit.php">
-            <?php if (isset($errorMessage)): ?>
+            <?php if(isset($errorMessage)): ?>
                 <p><?php echo e($errorMessage); ?></p>
             <?php endif; ?>
             <label class="guestbook-entry-label" for="name">Dein Name:</label>
-            <input
+            <input 
                 required="required"
                 class="guestbook-entry-input"
                 type="text"
@@ -27,20 +25,20 @@
                 name="name" />
 
             <label class="guestbook-entry-label" for="title">Titel des Eintrags:</label>
-            <input
+            <input 
                 required="required"
-                class="guestbook-entry-input"
-                type="text"
+                class="guestbook-entry-input" 
+                type="text" 
                 id="title"
                 name="title" />
 
             <label class="guestbook-entry-label" for="content">Inhalt des Eintrags:</label>
-            <textarea
+            <textarea 
                 required="required"
                 rows="4"
-                class="guestbook-entry-input"
-                type="text"
-                id="content"
+                class="guestbook-entry-input" 
+                type="text" 
+                id="content" 
                 name="content"></textarea>
 
             <div class="guestbook-form-buttons">
@@ -53,16 +51,16 @@
 
         <pre><?php print_r($entries); ?></pre>
 
-        <?php foreach ($entries as $entry): ?>
+        <?php foreach($entries AS $entry): ?>
             <?php
-            $paragraphs = explode("\n", $entry['content']);
-            $filteredParagraphs = [];
-            foreach ($paragraphs as $paragraph) {
-                $paragraph = trim($paragraph);
-                if (strlen($paragraph) > 0) {
-                    $filteredParagraphs[] = $paragraph;
+                $paragraphs = explode("\n", $entry['content']);
+                $filteredParagraphs = [];
+                foreach ($paragraphs AS $paragraph) {
+                    $paragraph = trim($paragraph);
+                    if (strlen($paragraph) > 0) {
+                        $filteredParagraphs[] = $paragraph;
+                    }
                 }
-            }
             ?>
             <div class="guestbook-entry">
                 <div class="guestbook-entry-header">
@@ -74,16 +72,35 @@
                     </span>
                 </div>
                 <div class="guestbook-entry-content">
-                    <?php foreach ($filteredParagraphs as $p): ?>
+                    <?php foreach($filteredParagraphs AS $p): ?>
                         <p><?php echo e($p); ?></p>
                     <?php endforeach; ?>
                 </div>
             </div>
         <?php endforeach; ?>
 
+        
+        <?php var_dump($perPage); ?><br />
+        <?php var_dump($countTotal) ?>
 
+        <?php
+            $numPages = ceil($countTotal / $perPage);
+            var_dump($numPages);
 
+        ?>
+        
         <ul class="guestbook-pagination">
+            <?php for($x = 1; $x <= $numPages; $x++): ?>
+                <li class="guestbook-pagination-li">
+                    <a 
+                        class="guestbook-pagination-a" 
+                        href="index.php?<?php echo http_build_query(['page' => $x]); ?>">
+                        
+                        <?php echo e($x); ?>
+                    </a>
+                </li>
+            <?php endfor; ?>
+            <?php /*
             <li class="guestbook-pagination-li">
                 <a class="guestbook-pagination-a guestbook-pagination-active" href="#">1</a>
             </li>
@@ -96,15 +113,15 @@
             <li class="guestbook-pagination-li">
                 <a class="guestbook-pagination-a" href="#">4</a>
             </li>
+            */ ?>
         </ul>
 
         <hr class="guestbook-separator" />
 
         <footer class="guestbook-footer">
-            <p>Implementiert im PHP-Kurs</p>
+            <p>Gästebuch für Hotel Königshof</p>
         </footer>
 
     </div>
 </body>
-
 </html>
