a67993d983730ca31256c2e967e3f9d49ea0a77b
Simon <simonwilli7@web.de>
2025-06-12 11:23:16 +0200

    Optimierung und readme

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/inc/db-connect.php';
 require_once __DIR__ . '/inc/functions.php';
 
-$perPage = 5;
+$perPage = 2;
 $currentPage = 1;
 if (isset($_GET['page'])) {
     $currentPage = @(int) $_GET['page'];
@@ -21,9 +21,6 @@ $countTotal = $stmtCount->fetch(PDO::FETCH_ASSOC)['count'];
 
 $stmt = $pdo->prepare('SELECT * FROM `entries` ORDER BY `id` DESC LIMIT :offset, :perPage');
 $stmt->bindValue('perPage', $perPage, PDO::PARAM_INT);
-// Seite 1: Offset 0
-// Seite 2: Offset $perPage
-// Seite 3: Offset $perPage * 2
 $stmt->bindValue('offset', ($currentPage - 1) * $perPage, PDO::PARAM_INT);
 $stmt->execute();
 $entries = $stmt->fetchAll(PDO::FETCH_ASSOC);
---
+++ readme.txt
@@ -0,0 +1,23 @@
+Projekt Gästebuch mit PHP 
+mit Datenbankanbindung 
+von Najeb und Simon
+
+Plan / Feature
+
+Prio 1-6 (1 == hoch)
+
+Muss
+001	HTML-Struktur mit CSS 1
+002	Datenbank einrichten 1
+003	Verbindung zur Datenbank 1
+004	Programmierung für das Absenden des Formulars 1
+005	Paginierung einfach 2
+
+Kann
+006	Paginierung detailliert 4
+007	Git-Hub Verknüpfung 3
+008 	Formatierung 6
+009	Kommentierung 4
+
+Umgesetzt
+001,002, 003, 004, 005, 006, 007
